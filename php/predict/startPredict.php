13b70b4f406f7b4e343055356ae3bc965b310281
Yusen Lin <412206186@qq.com>
2018-01-16 15:04:00 +0800

    [RC] modify

--- php/predict/startPredict.php
+++ php/predict/startPredict.php
@@ -1,14 +1,11 @@
 <?php
 
-//if(!isset($_COOKIE['username']) || !isset($_GET['symbol'])) {
-//    echo 0;
-//    exit;
-//}
-//$unique_id = $_COOKIE['username'];
-//$symbol = $_GET['symbol'];
-
-$unique_id = 'test123';
-$symbol = 'sh601988';
+if(!isset($_COOKIE['username']) || !isset($_GET['symbol'])) {
+    echo 0;
+    exit;
+}
+$unique_id = $_COOKIE['username'];
+$symbol = $_GET['symbol'];
 
 $symbol = preg_replace('/[^szh0-9]/', '', $symbol);
 
@@ -19,6 +16,9 @@ if(preg_match('/Windows/', php_uname('s'), $matches)) {
 } else {
     $exec = "cd $dir_path;python run.py " . $unique_id . ' ' . $symbol;
 }
+
+var_dump($symbol, $unique_id, $exec);
+
 exec($exec, $out_put);
 
 foreach($out_put as $value)
